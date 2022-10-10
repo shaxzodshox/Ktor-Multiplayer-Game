92f1d5347e099566b21eb4e3b9344317ecc8be8c
shaxzodshox <developer.shakhzod@gmail.com>
2022-10-10 01:27:56 +0100

    Game phases are made

--- src/main/kotlin/uk/shakhzod/data/Room.kt
+++ src/main/kotlin/uk/shakhzod/data/Room.kt
@@ -8,6 +8,33 @@ class Room(
     val maxPlayers: Int,
     var players: List<Player> = listOf()
 ) {
+
+    private var phaseChangedListener: ((Phase) -> Unit)? = null
+    var phase = Phase.WAITING_FOR_PLAYERS
+    set(value) {
+        synchronized(field){
+            field = value
+            phaseChangedListener?.let { change -> change(value) }
+        }
+    }
+
+    private fun setPhaseChangedListener(listener: (Phase) -> Unit){
+        phaseChangedListener = listener
+    }
+
+    init {
+        setPhaseChangedListener {
+            newPhase ->
+            when(newPhase){
+                Phase.WAITING_FOR_PLAYERS -> waitingForPlayers()
+                Phase.WAITING_FOR_START -> waitingForStart()
+                Phase.NEW_ROUND -> newRound()
+                Phase.GAME_RUNNING -> gameRunning()
+                Phase.SHOW_WORD -> showWord()
+            }
+        }
+    }
+
     //This method will broadcast the message to everyone connected..
     suspend fun broadcast(message: String) {
         players.forEach { player ->
@@ -30,5 +57,19 @@ class Room(
 
     fun containsPlayer(username: String) : Boolean{
         return players.find { it.username == username } != null
+    }
+
+    private fun waitingForPlayers(){}
+    private fun waitingForStart(){}
+    private fun newRound(){}
+    private fun gameRunning(){}
+    private fun showWord(){}
+
+    enum class Phase{
+        WAITING_FOR_PLAYERS,
+        WAITING_FOR_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
     }
 }
